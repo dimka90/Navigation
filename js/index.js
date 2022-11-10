fdb11ee367259fc5a2aa13512475b32f6c2dabb5
dimka90 <dimkayilrit@gmail.com>
2022-11-10 15:11:14 +0100

    added the harmburger menu fuctionalit

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const navigation=document.querySelector('.primary-navigation');
+const btnToggle=document.querySelector('.btn-img');  
+
+
+btnToggle.addEventListener("click",()=>{
+    const visible=navigation.getAttribute('data-visible');
+    const image=document.getElementById('harmburger-menu');
+  
+    console.log(visible);
+    if (visible==="false"){
+    
+        navigation.setAttribute('data-visible',true);
+        image.setAttribute('src','/images/close-menu.png');
+    }
+    else if(visible==="true"){
+        navigation.setAttribute('data-visible',false);  
+        image.setAttribute('src','/images/menu.png');
+    }
+
+});
